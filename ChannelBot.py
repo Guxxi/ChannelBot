09b18535dcdf8aea37f979c3d795d6f9a76c7b11
Guxxi <38870850+Guxxi@users.noreply.github.com>
2018-05-07 09:50:26 -0400

    Update ChannelBot.py

--- ChannelBot.py
+++ ChannelBot.py
@@ -16,23 +16,49 @@ async def on_voice_state_update(member, before, after):
     channelID = 440255311949987849
     categoryID = 440255201828536320
     if after.channel is not None:
-        if after.channel.id == channelID:
+        if after.channel.id == channelID and member =! in client.deniedMembers:
             client.channelowner = member
             client.newChannel = await member.guild.create_voice_channel('Rename Me!', category=client.get_channel(categoryID))
             await member.move_to(client.newChannel)
             overwrite = discord.PermissionOverwrite()
             overwrite.manage_channels = True
             await client.newChannel.set_permissions(client.channelowner, overwrite=overwrite)
-        elif before.channel.id == client.newChannel.id and after.channel.id ==
-        elif before.channel.id == client.newChannel.id and after.channel.id != client.newChannel.id and client.newChannel.members == []:
+        elif before.channel.id == client.newChannel.id and after.channel.id == client.newChannel.id:
+            await discord.VoiceChannel.delete(client.newChannel
+        elif before.channel.id == client.newChannel.id and after.channel.id =! client.newChannel.id and client.newChannel.members == []:
             await discord.VoiceChannel.delete(client.newChannel)
 
 @client.command()
 async def kick(ctx, member: discord.Member):
     if client.channelowner.id == ctx.author.id:
         afkChannel = client.get_channel(437834824393293824)
-        client.commandChannel = client.get_channel(440687280907354113)
         await member.move_to(afkChannel)
-        client.commandChannel.send("Kicked " + member.mention + " from" + client.channelowner.mention + "'s Channel!")
+        channel = client.get_channel(440687280907354113)
+        client.channel.send("Kicked " + member.mention + " from " + ctx.author.mention + "'s Channel!")
+
+@client.command()
+async def deny(ctx, member: discord.Member):
+    client.deniedMembers = []
+    if member =! in client.deniedMembers and client.channelowner.id == ctx.author.id:
+        client.deniedMembers.append(member)
+        afkChannel = client.get_channel(437834824393293824)
+        await member.move_to(afkChannel)
+        channel = client.get_channel(440687280907354113)
+        await channel.send("Denied access to " + member.mention + " from " + ctx.author.mention + "'s channel!")
+    elif member == in client.deniedMembers:
+        await channel.send(member.mention + " is already denied from " + ctx.author.mention + "'s channel!)
+    elif client.channelowner.id =! ctx.author.id:
+        await channel.send("You are not the owner of this channel!")
+
+@client.command()
+async def undeny(ctx, member: discord.Member):
+    if client.deniedMembers is not None and member == in client.deniedMembers and client.channelowner.id == ctx.author.id:
+        client.deniedMembers.remove(member)
+        channel = client.get_channel(440687280907354113)
+        await channel.send("Allowed access to " + member.mention + " to " + ctx.author.mention + "'s channel!")
+    elif member != client.deniedMembers:
+        await channel.send(member.mention + " was never denied!")
+    elif client.channelowner.id =! ctx.author.id:
+        await channel.send("You are not the owner of this channel!")
 """"""
 client.run("NDM3NDgxNzkxMTE3MzI4Mzk1.DcP8TQ.6iCLEPGYYMzt8ZfQxlIBjy-pJ60")
